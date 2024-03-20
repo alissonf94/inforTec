63650d82f8f063d00d6978002be94bbdcbb6d4c7
Alissonf94 <alissonfernandesxx55@gmail.com>
2024-03-20 16:14:07 -0300

    finaliy sphoppingCart

--- infortec-frontEnd/src/pages/ShoppingCart.js
+++ infortec-frontEnd/src/pages/ShoppingCart.js
@@ -4,17 +4,21 @@ import NavBar from "../components/NavBar"
 import ItemShoppingCart from "../components/ItemShoppingCart"
 import "../styles/ShoppingCart.css"
 import { CiBoxList } from "react-icons/ci";
+import { IoIosDocument } from "react-icons/io";
+
 const ShoppingCart = ()=>{
    const [itensProduct, setItensProduct] = useState([])
    const idClient = localStorage.getItem('userId')
-    const [valueBuy, setValueBuy] = useState(0) 
+   const [valueBuy, setValueBuy] = useState(0) 
+   
    async function getItemsProducts (){
         try {
             const result  = await ShoppingCartService.getItemsProducts(idClient)
 
             const data = await result.json()
             setItensProduct(data.itens)
-            setValueBuy(data.valueBuy)
+            const value = parseFloat(data.valueBuy).toFixed(2)
+            setValueBuy(value)
         } 
         catch (error) {
             
@@ -47,23 +51,25 @@ const ShoppingCart = ()=>{
                 </div>
                 
                 <div className="information-valueBuy">
-                    <span className="title-information-valueBuy">Resumo</span>
-                    <div>
-                        <span>Quant. de itens:</span>
-                        <span className="quantityItens"></span>
+                    <div className="title-information-valueBuy">
+                        <IoIosDocument className="icon-title" size={20} color="#327CAD"/>
+                        <span className="t">Resumo</span>
                     </div>
-                    <div>
-                        <span>Frete:</span>
-                        <span className="frete">R$ 0.00</span>
+                    
+                    <div className="quantityItens-information-valueBuy">
+                        <span>Quant. de itens: </span>
+                        <span className="value-quantityItens"> {itensProduct.length} </span>
+                    </div>
+                    <div className="frete-informatio-valueBuy" >
+                        <span >Frete:</span>
+                        <span className="value-frete">R$ 0.00</span>
                     </div>
                     <div className="valueBuy">
-                        <span >{valueBuy}</span>
+                        <span > R$ {valueBuy}</span>
                     </div>
-                    <div>
-                        <button>Finalizar compra</button>
-                    </div>
-                    <div>
-                        <button>Continuar comprando</button>
+                    <div className="buttons-information-valueBuy">
+                        <button>FINALIZAR A COMPRA</button>
+                        <button>CONTINUAR COMPRANDO</button>
                     </div>
                 </div>   
             </div>
